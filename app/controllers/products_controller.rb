63636625a51f639b590a4bb73f6e4baad9a2be32
Vytautas Leoncikas <v.leo@protonmail.com>
2019-11-26 16:42:59 +0000

    reload doesnt work

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,27 @@
 class ProductsController < ApplicationController
   before_action :set_user, only: [:index]
+
   def index
     @products = Products.all
     @user_products = @user.products
   end
 
   private
+
   def set_user
     @user = User.find(params[:id])
   end
+
+  def calculate_fav(products)
+    colour = {Black: 0, Pastel: 0,Bright: 0,Neutral: 0}
+    cut = {Tight: 0, Loose: 0}
+    type = {Sports_Bra: 0 , Leggings: 0, Crop_Top: 0, T_shirt: 0, Shorts: 0, Hoodie: 0}
+    products.each |product|
+      colour[product.colour.name.to_sym] += 1
+      cut[product.cut.name.to_sym] += 1
+      type[product.product_type.name.gsub(/\s+/,"_").to_sym] += 1
+
+    end
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   belongs_to :product_type
   belongs_to :colour
   belongs_to :cut
+  has_many :product_styles
+  has_many :styles, through: :product_styles
 
   validates :product_type, presence: true
   validates :price, presence: true
