af3808c6d81420f2090e50696b65e579108fd6ac
Vytautas Leoncikas <v.leo@protonmail.com>
2019-11-27 15:04:58 +0000

    Really bad personalised product search

--- app/models/style.rb
+++ app/models/style.rb
@@ -1,5 +1,5 @@
 class Style < ApplicationRecord
-
-
+  has_many :product_styles
+  has_many :products, through: :product_styles
   validates :name, presence: true, inclusion: { in: %w[Shoulders Belly Arms Waist Legs Glutes] }, uniqueness: true
 end
