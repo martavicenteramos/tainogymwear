711a8606e0f88b5a4ac3804b458f3365e0b84799
rafael soares <soares.pratas.r@gmail.com>
2019-11-28 14:47:52 +0000

    scrapper cloudinary seed picture model

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,61 @@
 # If you already have a db created please drop it and create a new with this
 # seed
 # require 'mechanize'
-# require_relative 'gymsharkwomen-scraper'
+class GymSharkWomenScraper
+  def self.scrape
+    products = []
+    agent = Mechanize.new
+    agent.user_agent_alias = 'Mac Safari'
+    limit = 1 # page limit in url
+    for page_number in 1..limit
+      url = URI("https://eu.gymshark.com/collections/all-products/womens?page=#{page_number}")
+      # Search each link inside a card
+      agent.get(url).search(".grid__item .prod-image-wrap a").each do |link|
+        ######TODO 1: will click the link and scrape data inside that link
+          agent_inside = Mechanize.new
+          agent_inside.user_agent_alias = 'Mac Safari'
+          inside_page_link = "https://eu.gymshark.com#{link[:href]}"
+          ## We enter the page for each link and save the data we want
+          current_agent = agent_inside.get(inside_page_link)
+          product = {
+            name: get_place_name(current_agent),
+            price: get_price_range(current_agent),
+            image: get_image(current_agent)
+          }
+          products << product
+      end
+    end
+    save(products)
+    products
+  end
+
+  private
+
+
+  def self.get_price_range(agent)
+    cost_element = agent.search('.pinfo__price__total').text
+    cost = cost_element.match(/\d+/)
+    cost && cost[0] != '' ? cost[0].to_f / 2 : 0
+  end
+  def self.get_place_name(agent)
+    return agent.search(".pinfo__heading").text
+  end
+
+  def self.get_image(agent)
+    image_array = []
+    agent.search(".pimages .pimages__image").each do |url|
+      image_array << url.values[2]
+    end
+    return image_array
+  end
+
+  def self.save(data)
+    File.open('gymsharkwomen.json', "w+") do |f|
+      f << data.to_json
+    end
+  end
+end
+
 
 OrderProduct.destroy_all
 Order.destroy_all
@@ -16,6 +70,8 @@ Colour.destroy_all
 Cut.destroy_all
 Style.destroy_all
 User.destroy_all
+Picture.destroy_all
+
 
 
 # Product Types
@@ -52,27 +108,36 @@ cleavage = Style.create(name: "Cleavage")
 # Initiate scraper
 clothes = GymSharkWomenScraper.scrape
 
-
 # dummy image
 # img = "https://images.unsplash.com/photo-1562886877-0be0db6aba84?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3289&q=80"
 
 
-# Products
+# Products & Picture & Cloudinary
 puts "Create all products"
-clothes[0..10].each do |st|
-    Product.create!(
+clothes.each do |st|
+   p = Product.create!(
     {
       name: st[:name],
       product_type: ProductType.all.sample,
       colour: Colour.all.sample,
       cut: Cut.all.sample,
-      price: st[:price],
-      image_url: st[:image]
-    }
-  )
-
+      price: st[:price]
+    })
+      st[:image].each do |img|
+        begin
+        uploaded_from_scrapper = Cloudinary::Uploader.upload(img, options = {})
+        Picture.create!(
+  {
+        url: uploaded_from_scrapper["secure_url"],
+        product_id: p.id
+  })
+          rescue => e
+          puts e
+      end
+  end
 end
 
+#Cloudinary & Picture
 
 # One style per product products
 puts "Adding styles to the products"
