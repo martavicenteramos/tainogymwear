f1df03450ea4461e4aebdb2e3da06aa958835bb1
Jaqueline Pinto <jaque86@gmail.com>
2019-12-03 18:40:01 +0000

    Questionary ok, without style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,29 @@
 class UsersController < ApplicationController
-
   def show
-    @user = current_user
-    authorize @user
-    @user_style = UserStyle.new
-    @cut = Cut.new
-    @user_colour = UserColour.new
-    @user_product_type = UserProductType.new
+    authorize current_user
   end
 
   def edit
   end
 
   def update
+    authorize current_user
+
+    if current_user.update(user_params)
+      redirect_to profile_path
+    else
+      render :show
+    end
   end
 
   def create
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :cut_id, style_ids: [], product_type_ids: [], colour_ids: []
+    )
   end
 end
