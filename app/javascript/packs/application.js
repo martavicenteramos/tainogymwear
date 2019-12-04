84c729d2f7d882430625763d123f541c8f1d500e
Vytautas Leoncikas <v.leo@protonmail.com>
2019-12-04 12:01:24 +0000

    algolia should work on heroku with this code

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,7 @@
 import algoliasearch from "algoliasearch";
 import "bootstrap";
 
-var client = algoliasearch('IBVWT5Z8Y5', '92014f06c232528778ca6dea55cfb907');
+var client = algoliasearch('IBVWT5Z8Y5', 'cf50e0757b779100f4ea1e8822f923f5');
 var index = client.initIndex('Product');
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -22,6 +22,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+//rafas
+//application_id: 'IBVWT5Z8Y5',
+//api_key: 'cf50e0757b779100f4ea1e8822f923f5'
+//leos
+//'9S8X4M6OGV', 'c4250c1e9bd4adf63c9b8a8867df959f
+
 
 // document.addEventListener("DOMContentLoaded", function(){
 //   const addButtons = document.querySelectorAll(".btn-counter-add");
